d819e5c50f903e69888043c8d2d986dfd74c32ea
Imre Mehesz <imehesz@gmail.com>
2021-08-27 01:50:55 +0000

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+	import LockDropDown from "./LockDropDown.svelte"
+
+	let dataObj = {
+		archtype: [1,2,3],
+		flaws: [4,6,2],
+		adverbs: [3,2,1],
+		verts: [7,8,9],
+		roles: [9,8,7]
+	}
+</script>
+
+<main>
+	<h1>Character Aid</h1>
+
+	<button>Generate</button>
+
+	<LockDropDown name="Archtype" listItems="{ dataObj.archtype }" />
+	<LockDropDown name="Flaws" listItems="{ dataObj.flaws }" />
+	<LockDropDown name="Adverbs" listItems="{ dataObj.adverbs }"/>
+	<LockDropDown name="Verts" listItems="{ dataObj.verts }" />
+	<LockDropDown name="Roles" listItems="{ dataObj.roles }"/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/LockDropDown.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let name
+	export let listItems = []
+
+</script>
+
+<div class="lock-drop-down">
+	<fieldset>
+		<legend>{ name }</legend>
+		<select>
+			{#each listItems as option}
+			<option>{ option }</option>
+			{/each} 
+		</select>
+		Lock <input type="checkbox">
+	</fieldset>
+</div>
